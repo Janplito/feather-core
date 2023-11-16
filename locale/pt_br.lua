8ef45722bdf66ed8a0dfa42d9d7d2a46285f2519
Janplito <149108525+Janplito@users.noreply.github.com>
2023-11-16 04:42:01 -0300

    Create pt_br.lua

---
+++ locale/pt_br.lua
@@ -0,0 +1,11 @@
+LocalesAPI.register("pt_br", {
+    loadscreen_title = "Carregando o mundo...",
+    loadscreen_subtitle = "Nestas terras áridas do Velho Oeste, cada momento de carregamento se entrelaça com lendas contadas em torno das fogueiras crepitantes.",
+    loadscreen_signature = "-Alguém, provavelmente",
+    spawn_welcome = "Bem-vindo ao Feathercore!",
+    death_timer = "segundos restantes",
+    death_text = "Ressurreição imediata",
+    death_prompt = "Pressione e mantenha",
+    ui_settings_title = "Configurações",
+    ui_settings_locale_title = "Idioma"    
+})
